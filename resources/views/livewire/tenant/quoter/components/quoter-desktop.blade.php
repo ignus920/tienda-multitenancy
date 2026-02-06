8f947801705fd401177b2124c6ea92013ac1e795
jslozanoc10 <jslozanoc10@gmail.com>
2026-02-06 10:58:03 -0500

    prueba quitar text  main

--- resources/views/livewire/tenant/quoter/components/quoter-desktop.blade.php
+++ resources/views/livewire/tenant/quoter/components/quoter-desktop.blade.php
@@ -27,7 +27,7 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"></path>
                         </svg>
                     </div>
-                    <span>Nueva Cotización main</span>
+                    <span>Nueva Cotización</span>
                 </button>
             </div>
         </div>
