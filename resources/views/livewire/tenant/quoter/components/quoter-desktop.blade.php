eb951adf73bac87316771d7479a6e20dd71b1327
jslozanoc10 <jslozanoc10@gmail.com>
2026-02-06 10:46:06 -0500

    prueba main

--- resources/views/livewire/tenant/quoter/components/quoter-desktop.blade.php
+++ resources/views/livewire/tenant/quoter/components/quoter-desktop.blade.php
@@ -27,7 +27,7 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"></path>
                         </svg>
                     </div>
-                    <span>Nueva Cotización</span>
+                    <span>Nueva Cotización main</span>
                 </button>
             </div>
         </div>
