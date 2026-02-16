ebe363bd5eea59735690e21a30a10ddb3cc5ea27
jslozanoc10 <jslozanoc10@gmail.com>
2026-02-16 16:07:51 -0500

    tenantManager
    db

--- app/Services/Tenant/TenantManager.php
+++ app/Services/Tenant/TenantManager.php
@@ -522,7 +522,7 @@ class TenantManager
             'driver' => 'mysql',
             'host' => $tenant->db_host,
             'port' => $tenant->db_port,
-            'database' => 'desarrollo',//$tenant->db_name,
+            'database' => $tenant->db_name,
             'username' => $tenant->db_user,
             'password' => $tenant->db_password,
             'charset' => 'utf8mb4',
