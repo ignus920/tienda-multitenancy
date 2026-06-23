82071b74d1e5c7ec61c084a5464171415df29bbb
ignus920 <83846480+ignus920@users.noreply.github.com>
2026-06-23 15:27:44 -0500

    pruebas dos

--- app/Services/Tenant/WordPress/WordPressService.php
+++ app/Services/Tenant/WordPress/WordPressService.php
@@ -574,7 +574,7 @@ class WordPressService
 
         $precioWP = 0;
         if ($precioRecord && (float) $precioRecord->values > 0) {
-            $taxRate  = (float) ($item->tax?->percentage ?? 0);
+            $taxRate  = 0; // Omitir IVA temporalmente para pruebas
             $precioWP = round((float) $precioRecord->values * (1 + $taxRate / 100) / 10) * 10;
         }
 
