20232e3312b67dff46d3c7e56c20e5a993ab452a
ignus920 <83846480+ignus920@users.noreply.github.com>
2026-08-12 10:37:49 -0500

    Implementar Cant. Máxima, cálculo especial de empaque anulado al superar máximo y Nota de Bodega en remisión

--- app/Livewire/Tenant/Items/ManageDimensions.php
+++ app/Livewire/Tenant/Items/ManageDimensions.php
@@ -22,6 +22,8 @@ class ManageDimensions extends Component
     public $min_packing_qty;
     public $min_packing_val;
     public $add_packing_val;
+    public $max_packing_qty;
+    public $packing_note;
 
     public function mount($itemId)
     {
@@ -44,7 +46,9 @@ class ManageDimensions extends Component
             'quntityxbox' => $this->quntityxbox,
             'min_packing_qty' => $this->min_packing_qty ?: 0,
             'min_packing_val' => $this->min_packing_val ?: 0.00,
-            'add_packing_val' => $this->add_packing_val ?: 0.00
+            'add_packing_val' => $this->add_packing_val ?: 0.00,
+            'max_packing_qty' => $this->max_packing_qty ?: 0,
+            'packing_note' => $this->packing_note ?: null
         ];
         //dd($infoItem);
         try {
@@ -91,6 +95,8 @@ class ManageDimensions extends Component
             $this->min_packing_qty = $itemDimension->min_packing_qty;
             $this->min_packing_val = $itemDimension->min_packing_val;
             $this->add_packing_val = $itemDimension->add_packing_val;
+            $this->max_packing_qty = $itemDimension->max_packing_qty;
+            $this->packing_note = $itemDimension->packing_note;
         }
     }
 
@@ -132,6 +138,8 @@ class ManageDimensions extends Component
         $this->min_packing_qty = '';
         $this->min_packing_val = '';
         $this->add_packing_val = '';
+        $this->max_packing_qty = '';
+        $this->packing_note = '';
         $this->itemId = '';
         $this->dimensions_id = '';
     }
