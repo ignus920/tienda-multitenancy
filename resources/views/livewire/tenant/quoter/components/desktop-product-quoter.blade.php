08dce199db8eadda78412d32fd28ceb214e1c9e8
ignus920 <83846480+ignus920@users.noreply.github.com>
2026-08-13 12:57:14 -0500

    actualikzdo

--- resources/views/livewire/tenant/quoter/components/desktop-product-quoter.blade.php
+++ resources/views/livewire/tenant/quoter/components/desktop-product-quoter.blade.php
@@ -1576,7 +1576,6 @@
                         <svg class="w-6 h-6 mr-3 text-blue-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 009.586 13H7"></path>
                         </svg>
-                        Selecciona una sucursal de envío
                     </h3>
                     <p class="text-sm text-gray-500 dark:text-slate-400 mt-1">
                         Selecciona la sucursal de envio
