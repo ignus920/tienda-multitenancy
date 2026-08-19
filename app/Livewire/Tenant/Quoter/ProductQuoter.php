32cada8f061183d4e23d129e7c9b56e5de593b0b
ignus920 <83846480+ignus920@users.noreply.github.com>
2026-08-19 12:23:45 -0500

    feat: cambiar nombre de exportacion de excel a Informe_de_Vitrina_y_Cuarentena

--- app/Livewire/Tenant/Quoter/ProductQuoter.php
+++ app/Livewire/Tenant/Quoter/ProductQuoter.php
@@ -4625,7 +4625,7 @@ class ProductQuoter extends Component
             ];
         };
 
-        $filename = 'Reporte_Especial_Inventario_' . now()->format('Ymd_His') . '.xlsx';
+        $filename = 'Informe_de_Vitrina_y_Cuarentena_' . now()->format('Ymd_His') . '.xlsx';
 
         return \Maatwebsite\Excel\Facades\Excel::download(
             new \App\Exports\GenericExport($data, $headings, $mapping),
