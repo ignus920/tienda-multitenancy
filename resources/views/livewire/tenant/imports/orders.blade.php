d3866632f845396a5169494dfc15ca40bedb6006
ignus920 <83846480+ignus920@users.noreply.github.com>
2026-08-28 10:27:04 -0500

    actualñizado

--- resources/views/livewire/tenant/imports/orders.blade.php
+++ resources/views/livewire/tenant/imports/orders.blade.php
@@ -818,15 +818,6 @@
                                 <td x-show="showCols.comment" class="px-4 py-4">
                                     @if ($filterStatus == 14)
                                         <span class="text-xs text-gray-500 italic">N/A</span>
-                                    @elseif ($filterStatus == 13)
-                                        <div class="flex justify-center">
-                                            <button 
-                                                @click="$wire.openModalHistory({{ $order->id }})"
-                                                class="p-1.5 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 transition-colors focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 {{ isset($order->news) && $order->news == 1 ? 'animate-pulse' : '' }}"
-                                                title="{{ Auth::user()?->profile_id == 17 ? 'View history' : 'Ver historial' }}">
-                                                <x-heroicon-o-chat-bubble-left-ellipsis class="w-5 h-5 mr-1 inline-block" /> Chat
-                                            </button>
-                                        </div>
                                     @else
                                         <div class="space-y-2 min-w-[200px]">
                                             <div x-data="{ comment: '' }" class="flex items-center gap-1">
