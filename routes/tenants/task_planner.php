a78f5940bfbb60d935cb1d99e2c9470108403983
ignus920 <83846480+ignus920@users.noreply.github.com>
2026-09-08 11:45:13 -0500

    actualizado

--- routes/tenants/task_planner.php
+++ routes/tenants/task_planner.php
@@ -3,8 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Tenant\TaskPlanner\ManageTasks;
 use App\Livewire\Tenant\TaskPlanner\MyTasksToday;
+use App\Http\Controllers\Tenant\TaskPlanner\AttachmentController;
 
 Route::middleware(['auth', 'verified', 'tenant'])->group(function () {
     Route::get('/tenant/task-planner', ManageTasks::class)->name('tenant.task-planner');
     Route::get('/tenant/mis-tareas', MyTasksToday::class)->name('tenant.task-planner.my-tasks');
+
+    Route::get('/tenant/task-planner/adjunto/{attachment}', [AttachmentController::class, 'show'])
+        ->whereNumber('attachment')
+        ->name('tenant.task-planner.attachment');
 });
