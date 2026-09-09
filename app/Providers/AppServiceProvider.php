81787426da911ad75635bb7b90666a3678ebede7
ignus920 <83846480+ignus920@users.noreply.github.com>
2026-09-09 17:22:05 -0500

    ACTUALIZADO

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,6 +28,8 @@ class AppServiceProvider extends ServiceProvider
 
         // Register Livewire components
         Livewire::component('auth.enable2-f-a', \App\Auth\Livewire\Enable2FA::class);
+        Livewire::component('auth.verify2-f-a', \App\Auth\Livewire\Verify2FA::class);
+        Livewire::component('auth.select-tenant', \App\Auth\Livewire\SelectTenant::class);
 
         // Event listeners for global auditing
         \Illuminate\Support\Facades\Event::listen('eloquent.created: *', function ($eventName, array $data) {
