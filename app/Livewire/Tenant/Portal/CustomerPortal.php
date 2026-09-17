f69514539721ed4f30e2067d20d999eed55d8d8d
ignus920 <83846480+ignus920@users.noreply.github.com>
2026-09-17 16:04:01 -0500

    actualizado

--- app/Livewire/Tenant/Portal/CustomerPortal.php
+++ app/Livewire/Tenant/Portal/CustomerPortal.php
@@ -208,6 +208,7 @@ class CustomerPortal extends Component
                 'inv_items.handles_serial',
                 'inv_items.status',
                 'inv_items.generic',
+                'inv_items.is_cuttable',
                 'inv_items.created_at',
                 'inv_items.updated_at',
                 'inv_items.deleted_at'
